19fe8be5ffe69d13787d77f5a7a7cc47323b0182
lauLanez <laulanyumbayire@gmail.com>
2024-07-15 11:07:30 -0500

    public folder added

--- my-app/components/Navbar.tsx
+++ my-app/components/Navbar.tsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import Link from "next/link"
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className='border-2 border-red-500 flexBetween max-container
+    padding-container relative z-30 py-5'>
+        <Link href="/">
+        Home 
+        </Link>
+
+    </nav>
   )
 }
 
