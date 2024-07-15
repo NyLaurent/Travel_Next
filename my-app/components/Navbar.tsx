7a2db439151896965bdec7632131076aa78bcc20
lauLanez <laulanyumbayire@gmail.com>
2024-07-15 11:57:36 -0500

    button components created

--- my-app/components/Navbar.tsx
+++ my-app/components/Navbar.tsx
@@ -1,13 +1,32 @@
-import React from 'react'
+
 import Link from "next/link"
+import Image from 'next/image'
+import logo from '../public/hilink-logo.svg'
+import { NAV_LINKS } from "@/constants"
+import Button from '../components/Button'
+
 
 const Navbar = () => {
   return (
-    <nav className='border-2 border-red-500 flexBetween max-container
-    padding-container relative z-30 py-5'>
+    <nav className='flexBetween max-container
+    padding-container relative z-30 py-5 '>
         <Link href="/">
-        Home 
+        <Image src={logo} alt="logo" width={74} height={29}/>
+
+        
         </Link>
+        <ul className="hidden h-full gap-12 lg:flex">
+            {NAV_LINKS.map((link)=>(
+                <Link href={link.href} key={link.key} 
+                className="regular-16 text-gray-600 flexCenter
+                cursor-pointer pb-1.5 transition-all hover:font-bold">
+                    {link.label}
+                </Link>
+            ))}
+        </ul>
+        <div className="lg:flexCenter hidden">
+            <Button/>
+        </div>
 
     </nav>
   )
