533f40c749a57884fa5f64f3c891a41f03962705
lauLanez <laulanyumbayire@gmail.com>
2024-07-17 14:15:31 -0500

    created guide component

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from '../components/Hero'
 import Camp from '../components/camp'
+import Guide from '../components/Guide'
 
 export default function Home() {
   return (
    <>
    <Hero></Hero>
    <Camp></Camp>
+   <Guide></Guide>
    </>
   );
 }
