7f0dc343eb2ef4a417fc602d4bd09947bd99b5e6
lauLanez <laulanyumbayire@gmail.com>
2024-07-19 13:56:51 -0500

    last commit

--- my-app/components/Guide.tsx
+++ my-app/components/Guide.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 const Guide = () => {
   return (
    <section className='flexCenter flex-col'>
-    <div className='border-2 border-cyan-950 padding-container
+    <div className='padding-container
     max-container w-full pb-24'>
       <Image
       src="camp.svg"
@@ -24,8 +24,44 @@ const Guide = () => {
       </div>
 
     </div>
-    <div>
-      
+
+    <div className='flexCenter max-container relative w-full
+    '>
+      <Image
+      src='/boat.png'
+      alt='boat'
+      width={1440}
+      height={580}
+      className='w-full object-cover object-center 2xl:rounded-5xl'>
+
+      </Image>
+      <div className='absolute flex bg-white py-8 pl-5
+      pr-7 gap-3 rounded-3xl border shadow-md md:left-[5%] lg:top-20'>
+        <Image
+        src="/meter.svg"
+        alt='meter'
+        width={16}
+        height={158}
+        className='h-full w-auto'></Image>
+        <div className='flexBetween flex-col'>
+          <div className='flex w-full flex-col'>
+            <div className='flexBetween w-full'>
+              <p className='regular-16 text-gray-20'>Destination</p>
+              <p className='bold-16 text-green-50'>48 min</p>
+            </div>
+            <p className='bold-20 mt-2'>Aguas Calientes</p>
+          </div>
+          <div className='flex w-full flex-col'>
+           
+              <p className='regular-16 text-gray-20'>Start track</p>
+              
+            
+            <h4 className='bold-20 mt-2 whitespace-nowrap'>Wonorejo Pasuruan</h4>
+          </div>
+        </div>
+
+      </div>
+
     </div>
 
    </section>
