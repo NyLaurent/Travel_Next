edfcc9d5b26436c7e9fc9e9848c24ec7fc4845f5
lauLanez <laulanyumbayire@gmail.com>
2024-07-20 09:14:57 -0500

    added links

--- my-app/components/Footer.tsx
+++ my-app/components/Footer.tsx
@@ -1,32 +1,61 @@
-import React from 'react'
-import Link from 'next/link'
-import Image from 'next/image'
+
+"use client";
+
+import React from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+import { FOOTER_LINKS } from '@/constants';
 
 const Footer = () => {
   return (
-   <footer className='flexCenter mb-24'>
+    <footer className='flexCenter mb-24'>
+      <div className='padding-container max-container flex w-full flex-col gap-14'>
+        <div className='flex flex-col items-start justify-center gap-[10%] md:flex-row'>
+          <Link href="/" className='mb-10'>
+            <Image
+              src="/hilink-logo.svg"
+              alt='logo'
+              width={74}
+              height={29}
+            />
+          </Link>
 
-    <div className='padding-container max-container flex w-full
-    flex-col gap-14'>
-      <div className='flex flex-col items-start
-      justify-center gap-[10%]
-      md:flex-row'>
-        <Link href="/" className='mb-10'>
-        <Image
-        src="hilink-logo.svg"
-        alt='logo'
-        width={74}
-        height={29}/>
-        </Link>
-
-
-        <div></div>
+          <div className='flex flex-wrap gap-10 sm:justify-between md:flex-1'>
+            {FOOTER_LINKS.map((columns) => (
+              <FooterColumn
+                key={columns.title}
+                title={columns.title}
+              >
+                <ul className='regular-14 flex flex-col gap-4 text-gray-30'>
+                  {columns.links.map((link) => (
+                    <li key={link}>
+                      <Link href='/'>
+                        {link}
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+              </FooterColumn>
+            ))}
+          </div>
+        </div>
       </div>
+    </footer>
+  );
+};
 
-    </div>
-
-   </footer>
-  )
+interface FooterColumnProps {
+  title: string;
+  children: React.ReactNode;
 }
 
-export default Footer
+const FooterColumn = ({ title, children }: FooterColumnProps) => {
+  return (
+    <div className='flex flex-col gap-5'>
+      <h4 className='bold-18 whitespace-nowrap'>{title}</h4>
+      {children}
+    </div>
+  );
+};
+
+export default Footer;
