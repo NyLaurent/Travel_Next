80a92fc465ff6ff32e006aff9d01bd7397cb0cf7
lauLanez <laulanyumbayire@gmail.com>
2024-07-20 08:36:57 -0500

    created footer component

--- my-app/app/layout.tsx
+++ my-app/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import Navbar from '../components/Navbar'
+import Footer from "@/components/Footer";
 
 import "./globals.css";
 
@@ -19,6 +20,7 @@ export default function RootLayout({
       <body>
         <Navbar/>
         {children}
+        <Footer/>
       </body>
     </html>
   );
