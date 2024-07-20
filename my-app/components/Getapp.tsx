c0d5874a6ca5813e96ea6dd8c87bd3718b7cf477
lauLanez <laulanyumbayire@gmail.com>
2024-07-19 22:51:18 -0500

    finished sownload section

--- my-app/components/Getapp.tsx
+++ my-app/components/Getapp.tsx
@@ -5,6 +5,11 @@ const Getapp = () => {
    <section className='flexCenter w-full flex-col pb-[100px]'>
 
     <div className='get-app'>
+        <div className='z-20 flex w-full flex-1 flex-col
+        items-start justify-center gap-12'>
+            <h2 className='bold-40 lg:bold-64 xl:max-w-[320px]'>Get for free now!</h2>
+            <p className='regular-16 text-gray-10'>Available on iOS and Android</p>
+        </div>
 
     </div>
    </section>
