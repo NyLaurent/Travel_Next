ea52b7e05bcff5340063ce8c33b4faa1f2a63202
lauLanez <laulanyumbayire@gmail.com>
2024-07-20 08:32:39 -0500

    last commit on getapp component

--- my-app/components/Button.tsx
+++ my-app/components/Button.tsx
@@ -14,7 +14,7 @@ const Button = ({type,title,icon,variant,full}:ButtonProps) => {
     <button className={`flexCenter gap-3 rounded-full border ${variant} ${full && 'w-full'}`}
     type={type}>
         {icon && <Image src={icon} alt={title} width={24} height={24}/>}
-        <label className='bold-16 whitespace-nowrap'>{title}</label>
+        <label className='bold-16 whitespace-nowrap cursor-pointer'>{title}</label>
         
 
     </button>
--- my-app/components/Getapp.tsx
+++ my-app/components/Getapp.tsx
@@ -12,7 +12,8 @@ const Getapp = () => {
             <h2 className='bold-40 lg:bold-64 xl:max-w-[320px]'>Get for free now!</h2>
             <p className='regular-16 text-gray-10'>Available on iOS and Android</p>
 
-            <div>
+            <div className='flex w-full flex-col gap-3 whitespace-nowrap
+            xl:flex-row'>
                 <Button
                 type='button'
                 title='Apple Store'
@@ -28,6 +29,14 @@ const Getapp = () => {
                 full
                 />
             </div>
+        </div>
+        <div className='flex flex-1 items-center justify-end'>
+            <Image
+            src='/phones.png'
+            alt='phones'
+            width={550}
+            height={870}/>
+
         </div>
 
     </div>
