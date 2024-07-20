de4afa0ce166b0898b08c377cf92d0b31b47e5ff
lauLanez <laulanyumbayire@gmail.com>
2024-07-19 23:13:01 -0500

    modified button

--- my-app/components/Getapp.tsx
+++ my-app/components/Getapp.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import Button from './Button'
+import Image from 'next/image'
 
 const Getapp = () => {
   return (
@@ -9,6 +11,23 @@ const Getapp = () => {
         items-start justify-center gap-12'>
             <h2 className='bold-40 lg:bold-64 xl:max-w-[320px]'>Get for free now!</h2>
             <p className='regular-16 text-gray-10'>Available on iOS and Android</p>
+
+            <div>
+                <Button
+                type='button'
+                title='Apple Store'
+                icon='/apple.svg'
+                variant='btn_white'
+                full
+                />
+                 <Button
+                type='button'
+                title='Play Store'
+                icon='/android.svg'
+                variant='btn_dark_green_outline'
+                full
+                />
+            </div>
         </div>
 
     </div>
