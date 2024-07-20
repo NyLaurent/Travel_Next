4ab5de2fd824f7cd037d0dc09302979a548780f3
lauLanez <laulanyumbayire@gmail.com>
2024-07-20 08:46:25 -0500

    added a logo

--- my-app/components/Footer.tsx
+++ my-app/components/Footer.tsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+   <footer className='flexCenter mb-24'>
+
+    <div className='padding-container max-container flex w-full
+    flex-col gap-14'>
+      <div className='flex flex-col items-start
+      justify-center gap-[10%]
+      md:flex-row'>
+        <Link href="/" className='mb-10'>
+        <Image
+        src="hilink-logo.svg"
+        alt='logo'
+        width={74}
+        height={29}/>
+        </Link>
+
+
+        <div></div>
+      </div>
+
+    </div>
+
+   </footer>
   )
 }
 
